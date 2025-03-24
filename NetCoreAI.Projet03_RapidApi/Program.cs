18bd6a38034096edc9377d43db7185ca0027d6d4
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-24 14:03:08 +0300

    Net Core AI - C# .Net ile Yapay Zeka Entegrasyonları
    Bölüm 3 : Proje 3: Dışarıdan Harici Apiler ile Çalışmak (Rapid Api)
    Rapid Api Projesinin Oluşturulması,
    View Model Oluşturma,
    Rapid Api Üzerinden Alınan Verinin Console'da Gösterilmesi,
    Series'e Bağlı Değerlerin Listesi

---
+++ NetCoreAI.Projet03_RapidApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using NetCoreAI.Projet03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "0c961a5194msh8f9b8f4001388a5p1c0833jsn4e305cb370d8" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+   foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "-" + series.title + " - Film Puanı: " + series.rating + " - Yapım Yılı: " + series.year);
+    }
+}
+Console.ReadLine();
