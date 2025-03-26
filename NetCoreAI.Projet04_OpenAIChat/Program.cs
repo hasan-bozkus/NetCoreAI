3c088390830f1c42162a592c1682fe43b9b93383
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-26 12:37:39 +0300

    Net Core AI - C# .Net ile Yapay Zeka Entegrasyonları
    Bölüm 5: Proje 5: Speech To Text
    Projenin Oluşturulması ve Dosya Formatı,
    Sesten Metne Dönüştürme Kodları,
    Api Key Alma İşleminin Test Edilmesi

--- NetCoreAI.Projet04_OpenAIChat/Program.cs
+++ NetCoreAI.Projet04_OpenAIChat/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Data;
 using System.Text;
 using System.Text.Json;
 
@@ -6,51 +6,59 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var ApiKey = "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";
-
-        Console.WriteLine("Lütfen sorunuzu yazınız: (örnek: 'Mardinde hava kaç derece')");
-
-        var prompt = Console.ReadLine();
+        var ApiKey = "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";
         using var httpClinet = new HttpClient();
         httpClinet.DefaultRequestHeaders.Add("Authorization", $"Bearer {ApiKey}");
 
-        var requestBody = new
-        {
-            model = "gbt-3.5-turbo",
-            messages = new[]
-            {
-                new {role= "system", content = "You are a helpful assistant."},
-                new {role= "user", content = prompt}
-            },
-            max_tokens = 500000000
-        };
+        Console.WriteLine("Lütfen sorunuzu yazınız: (örnek: 'Mardinde hava kaç derece')");
 
-        var json=JsonSerializer.Serialize(requestBody);
-        var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-        try
+        while (true)
         {
-            var respnose = await httpClinet.PostAsync("https://api.openai.com/v1/chat/completions", content);
-            var responseString = await respnose.Content.ReadAsStringAsync();
-            if(respnose.IsSuccessStatusCode)
+            var prompt = Console.ReadLine();
+
+
+            var messages = new[]
             {
-                var result = JsonSerializer.Deserialize<JsonElement>(responseString);
-                var answer = result.GetProperty("choices")[0].GetProperty("message").GetProperty("content").GetString();
-                Console.WriteLine("Open AI'ın Cevabı: ");
+                new { role = "system", content = "You are a helpful assistant." },
+                new { role = "user", content = prompt}
+            };
+
+            var requestBody = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = messages,
+                max_tokens = 500
+            };
+
+            var json = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var respnose = await httpClinet.PostAsync("https://api.openai.com/v1/chat/completions", content);
+                var responseString = await respnose.Content.ReadAsStringAsync();
+                if (respnose.IsSuccessStatusCode)
+                {
+                    var result = JsonSerializer.Deserialize<JsonElement>(responseString);
+                    var answer = result.GetProperty("choices")[0].GetProperty("message").GetProperty("content").GetString();
+                    Console.WriteLine("Open AI'ın Cevabı: ");
                     Console.WriteLine(answer);
+                }
+                else
+                {
+                    Console.WriteLine($"Bir hata oluştu: {respnose.StatusCode}");
+                    Console.WriteLine(responseString);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine($"Bir hata oluştu: {respnose.StatusCode}");
-                Console.WriteLine(responseString);
+
+                Console.WriteLine($"Bir hata oluştu: {ex.Message}");
             }
-        }
-        catch (Exception ex)
-        {
 
-            Console.WriteLine($"Bir hata oluştu: {ex.Message}");
-        }
+            Console.WriteLine();
 
+        }
     }
 }
 
@@ -58,6 +66,6 @@ class Program
  Authorization Bearer 123456abcde
 
 benim keyim:
-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
+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
 
  */
---
+++ NetCoreAI.Projet05_OpenWhisperAudioTraskript/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Headers;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string apiKey = "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";
+        string audioFilePath = "ses1.mp3";
+
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            var form = new MultipartFormDataContent();
+
+            var audioContent = new ByteArrayContent(File.ReadAllBytes(audioFilePath));
+            audioContent.Headers.ContentType=MediaTypeHeaderValue.Parse("audio/mpeg");
+            form.Add(audioContent, "file", Path.GetFileName(audioFilePath));
+            form.Add(new StringContent("whisper-1"), "model");
+
+            Console.WriteLine("Ses dosyası işleniyor, Lütfen bekleyiniz...");
+
+            var response = await client.PostAsync("https://api.openai.com/v1/audio/transcriptions", form);
+            if(response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Transkript: ");
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine($"Hata: {response.StatusCode}");
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
+/*
+ Api Key: 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
+ */
