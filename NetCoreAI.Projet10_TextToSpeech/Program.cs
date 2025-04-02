f36bd4a4e74c2d3f2daa8d21fabf434cfecfd1cd
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-04-02 20:51:48 +0300

    Net Core AI - C# .Net ile Yapay Zeka Entegrasyonları
    Bölüm 10: Proje 10: Text To Speech İşlemi
    Text To Speech Kütüphanesi,
    Text To Speech İşlemi

---
+++ NetCoreAI.Projet10_TextToSpeech/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = -1;
+
+        Console.Write("Menti Girin: ");
+        string input;
+        input = Console.ReadLine();
+        if(!string.IsNullOrEmpty(input))
+        {
+            speechSynthesizer.Speak(input);
+        }
+
+        Console.ReadKey();
+    }
+}
